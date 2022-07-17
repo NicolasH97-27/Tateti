729b1ce30f6d191c6451386a42f8f684be64106d
NicolasH97-27 <59940232+NicolasH97-27@users.noreply.github.com>
2022-07-17 15:57:02 -0300

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
+
+import { toBeInTheDOM, toHaveFormValues } from '@testing-library/jest-dom/dist/matchers';
+import { useState } from 'react'
 import './App.css';
 
+import Notification from './components/Notification';
+
 function App() {
+  const [turno,setTurno] = useState ("x");
+  const [square,setSquare] = useState(["","","","","","","","",""]);
+  const [disable, setDisable] = useState(false);
+  const [notification, setNotification] = useState("Buenos dias");
+  
+  
+
+  const rules2Win = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  
+  const onClick = (index) =>{
+    
+    const arraySquares = square.map((a,i) => i === index ? turno : a)
+    
+    setTurno(turno==="x" ? "o" : "x")
+
+    setSquare(arraySquares)
+
+    console.log(arraySquares)
+
+    const verificarX = arraySquares.map(squere => {
+      return(squere === "x")
+    })
+
+    const verificarO = arraySquares.map(squere => {
+      return(squere === "o")
+    })
+
+    
+    rules2Win.forEach((rules)=>{
+        let verificadoX = []
+        let verificadoO = []
+
+        rules.forEach((index)=>{
+            verificadoX.push(verificarX[index])
+            verificadoO.push(verificarO[index])
+        })
+
+        let ganoX = verificadoX.reduce((acc,red)=>{
+              if( acc === false ) return false 
+              if (red === true ) return acc = true
+              else return acc = false 
+        })
+
+        let ganoO = verificadoO.reduce((acc,red)=>{
+              if( acc === false ) return false 
+              if (red === true ) return acc = true
+              else return acc = false 
+        })
+        
+        if(ganoX) {
+          setNotification("ganaste X")
+          setDisable(true)
+        }
+        if(ganoO) {
+          setNotification("ganaste O")
+          setDisable(true)
+        }
+          
+
+
+        
+    })
+
+  }
+  const mensaje = notification
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='juego'>
+      <h1>TATETI</h1>
+
+
+      <div className='fila'>
+        <div>
+          <button disabled={disable} onClick={() => onClick(0)}>{square[0]}</button>
+          <button disabled={disable} onClick={() => onClick(1)}>{square[1]}</button>
+          <button disabled={disable} onClick={() => onClick(2)}>{square[2]}</button>
+        </div>
+        <div>
+          <button disabled={disable} onClick={() => onClick(3)}>{square[3]}</button>
+          <button disabled={disable} onClick={() => onClick(4)}>{square[4]}</button>
+          <button disabled={disable} onClick={() => onClick(5)}>{square[5]}</button>
+        </div>
+        <div>
+          <button disabled={disable} onClick={() => onClick(6)}>{square[6]}</button>
+          <button disabled={disable} onClick={() => onClick(7)}>{square[7]}</button>
+          <button disabled={disable} onClick={() => onClick(8)}>{square[8]}</button>
+        </div>
+      </div>
+      
+
+      <Notification 
+        nota={notification}
+      
+      />
     </div>
   );
 }
---
+++ src/components/Notification.js
@@ -0,0 +1,7 @@
+
+
+const Notification = ({nota}) =>{
+    return(
+      <h1>{nota}</h1>    )
+}
+export default Notification;
