a83781a059bedffae13e2041ba331089e793eee1
NicolasH97-27 <59940232+NicolasH97-27@users.noreply.github.com>
2022-07-18 17:07:03 -0300

    heroku final tateti

--- src/App.js
+++ src/App.js
@@ -1,120 +1,89 @@
-
-import { toBeInTheDOM, toHaveFormValues } from '@testing-library/jest-dom/dist/matchers';
 import { useState } from 'react'
 import './App.css';
 
 import Notification from './components/Notification';
 
-function App() {
-  const [turno,setTurno] = useState ("x");
-  const [square,setSquare] = useState(["","","","","","","","",""]);
-  const [disable, setDisable] = useState(false);
-  const [notification, setNotification] = useState("Buenos dias");
-  
-  
+const EMPTY = '';
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
 
-  const rules2Win = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+function App() {
+  const [turno, setTurno] = useState(PLAYER_X);
+  const [squares, setSquares] = useState(new Array(9).fill(EMPTY));
+  const [disable, setDisable] = useState(false);
+  const [notification, setNotification] = useState("");
+
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ]
 
-  
-  const onClick = (index) =>{
-    
-    const arraySquares = square.map((a,i) => i === index ? turno : a)
-    
-    setTurno(turno==="x" ? "o" : "x")
+  const onClick = (index) => {
+    if (squares[index] !== EMPTY) {
+      return;
+    }
 
-    setSquare(arraySquares)
+    const newSquares = squares.map((cellValue, i) => i === index ? turno : cellValue);
 
-    console.log(arraySquares)
+    const [newNotification, newDisable] = [winningPositions.map(winningPosition => [
+      winningPosition.map(x => newSquares[x] === PLAYER_X).every(x => x),
+      winningPosition.map(x => newSquares[x] === PLAYER_O).every(x => x)
+    ]).reduce(([currentCalculatedX, currentCalculatedO], [calculatedX, calculatedO]) =>
+      !currentCalculatedX && calculatedX
+        ? [true, currentCalculatedO]
+        : !currentCalculatedO && calculatedO
+          ? [currentCalculatedX, true]
+          : [currentCalculatedX, currentCalculatedO]
+      , [false, false])]
+      .map(([winX, winO]) => 
+        winX
+          ? ['Ganaste X', true]
+          : winO
+            ? ['Ganaste O', true]
+            : newSquares.filter(x => x === EMPTY).length === 0
+              ? ['Empate', true]
+              : [notification, disable]
+      ).flatMap(x => x);
 
-    const verificarX = arraySquares.map(squere => {
-      return(squere === "x")
-    })
+    const newTurno = turno === PLAYER_X ? PLAYER_O : PLAYER_X;
 
-    const verificarO = arraySquares.map(squere => {
-      return(squere === "o")
-    })
-    
-    rules2Win.forEach((rules)=>{
-        let verificadoX = []
-        let verificadoO = []
-
-        rules.forEach((index)=>{
-            verificadoX.push(verificarX[index])
-            verificadoO.push(verificarO[index])
-        })
-
-        let ganoX = verificadoX.reduce((acc,red)=>{
-              if( acc === false ) return false 
-              if (red === true ) return acc = true
-              else return acc = false 
-        })
-
-        let ganoO = verificadoO.reduce((acc,red)=>{
-              if( acc === false ) return false 
-              if (red === true ) return acc = true
-              else return acc = false 
-        })
-        
-        if(ganoX) {
-          setNotification("ganaste X")
-          setDisable(true)
-        }
-        if(ganoO) {
-          setNotification("ganaste O")
-          setDisable(true)
-        }
-
-        console.log(square.includes(""))
-        
-        if( square.filter(i => i === "").length <= 1 && !disable) {
-          setNotification("empate")
-          setDisable(true)
-
-        }
-          
-
-
-        
-    })
-
+    setTurno(newTurno)
+    setSquares(newSquares)
+    setNotification(newNotification);
+    setDisable(newDisable);
   }
-  const mensaje = notification
+
   return (
     <div className='juego'>
       <h1>TATETI</h1>
 
-
       <div className='fila'>
         <div>
-          <button disabled={disable} onClick={() => onClick(0)}>{square[0]}</button>
-          <button disabled={disable} onClick={() => onClick(1)}>{square[1]}</button>
-          <button disabled={disable} onClick={() => onClick(2)}>{square[2]}</button>
+          <button disabled={disable} onClick={() => onClick(0)}>{squares[0]}</button>
+          <button disabled={disable} onClick={() => onClick(1)}>{squares[1]}</button>
+          <button disabled={disable} onClick={() => onClick(2)}>{squares[2]}</button>
         </div>
         <div>
-          <button disabled={disable} onClick={() => onClick(3)}>{square[3]}</button>
-          <button disabled={disable} onClick={() => onClick(4)}>{square[4]}</button>
-          <button disabled={disable} onClick={() => onClick(5)}>{square[5]}</button>
+          <button disabled={disable} onClick={() => onClick(3)}>{squares[3]}</button>
+          <button disabled={disable} onClick={() => onClick(4)}>{squares[4]}</button>
+          <button disabled={disable} onClick={() => onClick(5)}>{squares[5]}</button>
         </div>
         <div>
-          <button disabled={disable} onClick={() => onClick(6)}>{square[6]}</button>
-          <button disabled={disable} onClick={() => onClick(7)}>{square[7]}</button>
-          <button disabled={disable} onClick={() => onClick(8)}>{square[8]}</button>
+          <button disabled={disable} onClick={() => onClick(6)}>{squares[6]}</button>
+          <button disabled={disable} onClick={() => onClick(7)}>{squares[7]}</button>
+          <button disabled={disable} onClick={() => onClick(8)}>{squares[8]}</button>
         </div>
       </div>
-      
 
-      <Notification 
+
+      <Notification
         nota={notification}
-      
       />
     </div>
   );
